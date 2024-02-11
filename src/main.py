768cc410f20fd74bda5940c0899855e7d882cd8a
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2024-02-11 08:24:07 -0300

    feat(Controllers): add blueprints to app
    create and add blueprints to app with dynaconf

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+from dynaconf import FlaskDynaconf
 from flask import Flask
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    FlaskDynaconf(app=app, settings_files=['settings.yaml'])
+    app.config.load_extensions()
 
     return app
