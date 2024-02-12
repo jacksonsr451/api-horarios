213b7d71fe67f21939ecc91ce1d8cf7a272257f3
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2024-02-12 14:54:05 -0300

    refactor(Entities): remove imports
    removed import to uuid in entities

--- src/entities/restriction.py
+++ src/entities/restriction.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, ForeignKey, String
 
+from src.entities.services.id_validator import IdValidator
+
 from .base import Base
 
 
@@ -10,3 +12,15 @@ class Restriction(Base):
     teacher_id = ForeignKey('teachers.id')
     day = Column(String)
     time = Column(String)
+
+    def __init__(
+        self,
+        id: str = None,
+        teacher_id: str = None,
+        day: str = None,
+        time: str = None,
+    ) -> None:
+        self.id = IdValidator.validate(id)
+        self.teacher_id = IdValidator.validate(teacher_id)
+        self.day = day
+        self.time = time
